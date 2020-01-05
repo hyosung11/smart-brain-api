1df75b644a01c2d56f2d1a59bfaabaa23ccf69a4
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-05 10:16:02 -0500

    add jsonwebtoken to package.json

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,52 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+  return db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
+          .then(user => user[0])
+          .catch(err => Promise.reject('unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        Promise.reject('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => Promise.reject('wrong credentials'))
+}
+
+const getAuthTokenId = () => {
+  console.log('auth ok');
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  // change to process.env.JWT_SECRET
+  return jwt.sign({ jwtPayload, 'JWT_SECRET', { expiresIn: '2 days'}});
+}
+
+const createSessions = (user) => {
+  // JWT token, return user data
+  const { email, id } = user;
+  const token = signToken(email);
+}
+
+const sigininAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokenId() :
+    handleSignin(db, bcrypt, req, res)
+      .then(data => {
+        data.id && data.email ? createSessions(data) : Promise.reject(data)
+      })
+      .catch(err => res.status(400).json(err))
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  sigininAuthentication: sigininAuthentication
 }
